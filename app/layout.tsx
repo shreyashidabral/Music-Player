32dc05e6067cddd987fe9deaf4f1d5850cf939ac
Shreyashi Dabral <shreyashidabral@gmail.com>
2023-11-04 13:14:06 +0530

    Setup Sidebar and Content layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
+import Sidebar from '@/components/Sidebar'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Music Player',
-  description: 'Explore and listen to your favourite music!',
+  description: 'Explore and listen to your favourite music!'
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
   )
 }
